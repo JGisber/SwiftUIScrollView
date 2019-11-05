76e1a3cfa3f1667e9e26a2f5f1f416c38652fb5a
Josue <jgisber@gmail.com>
2019-11-04 22:15:23 -0700

    Initial Commit

---
+++ SwiftUIScrollView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIScrollView
+//
+//  Created by Josue Gisber on 11/4/19.
+//  Copyright © 2019 Mpixel Design & Development. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
